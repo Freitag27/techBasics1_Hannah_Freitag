0f1e1cae6358eaad819e020dcfa3b874739de7e4
Freitag27 <hannah.l.s.freitag@stud.leuphana.de>
2026-04-19 00:52:54 +0200

    Update assignment1.py

--- assignments/week1/assignment1.py
+++ assignments/week1/assignment1.py
@@ -1,6 +1,4 @@
-#"Ideas: I really like Miffy, therefore I wanted to try and make a little art piece of her. 
-      #I wanted to put her into a scene with tulips, since she's often depicted with them.
-#I used images of the characters as reference, but i did not copy an exact image. I used () to find the different symbols.
+
 print('''
 
                     ............                                           ☀︎☀︎☀︎
